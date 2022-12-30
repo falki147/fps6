329b17dc1224565cd697413fa59a88e9bd8ded89
Florian Preinfalk <preinfalkflorian@gmail.com>
2022-12-30 18:57:39 +0100

    Improve menu

--- src/Scenes/Menu.cpp
+++ src/Scenes/Menu.cpp
@@ -12,8 +12,9 @@ MenuScene::MenuScene() {
 	m_batch.attribute(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), 12);
 	m_batch.endAttributes();
 
-	renderMenuSprite(0, vec2(50, 50));
-	renderMenuSprite(1, vec2(50, 200));
+	renderMenuSprite(2, vec2(0, 0));
+	renderMenuSprite(0, vec2(20, 370));
+	renderMenuSprite(1, vec2(20, 480));
 
 	m_batch.upload();
 }
@@ -22,9 +23,9 @@ void MenuScene::update() {
 	if (Window::wasMouseReleased()) {
 		auto x = Window::mouseX(), y = Window::mouseY();
 
-		if (x >= 50 && x <= 350 && y >= 50 && y <= 150)
+		if (x >= 20 && x <= 320 && y >= 370 && y <= 470)
 			Game::Goto(new MainScene(Level(Game::getResourceHandler()->getLevel())));
-		else if (x >= 50 && x <= 350 && y >= 200 && y <= 300) {
+		else if (x >= 20 && x <= 320 && y >= 480 && y <= 580) {
 			std::srand((unsigned int) std::time(nullptr));
 			auto grid = Dungeon::create({ 21, 21, 50, 2, 0.7f, 0.01f });
 			Game::Goto(new MainScene(Level(grid)));
@@ -34,10 +35,13 @@ void MenuScene::update() {
 
 void MenuScene::render() {
 	m_prog.use();
-	m_prog.setMVP(mat4::createOrthographic(0, 640, 480, 0, -1, 1));
+	m_prog.setMVP(mat4::createOrthographic(0, 800, 600, 0, -1, 1));
 
 	Game::getResourceHandler()->getSprite()[SpriteCollection::SpriteMenu].bindStage(0);
+	
+	glDisable(GL_DEPTH_TEST);
 	m_batch.render();
+	glEnable(GL_DEPTH_TEST);
 }
 
 void MenuScene::onKeyDown(SDL_Scancode code, SDL_Keycode key) { }
